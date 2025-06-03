32111385ab5a9cf8363c13b868c7d2609c4aa89f
Elenamena <menaromero.elena@gmail.com>
2025-06-03 12:23:05 +0200

    boton2 y error

--- EMPRESA_1ºDAW/JQUERY/ejemplo1/main.js
+++ EMPRESA_1ºDAW/JQUERY/ejemplo1/main.js
@@ -1,20 +1,26 @@
 
-const listUsers=()=> {
+const listUsers = () => {
     $.ajax( {
         type: 'GET',
         url: 'https://jsonplaceholder.typicode.com/users',
         contentType: 'application/json',
         async: true,
-        success: function(data) {},
+        success: function(data) {
+            $('#userList').empty();
+
+            data.forEach(usuario => {
+                $('#userList').append(
+                    `<li><strong>${usuario.name}</strong> (${usuario.email})</li>`
+                );
+            });
+        },
         error: function(error) {
             alert('Error al obtener los usuarios');
+            console.error('Error:', error);
         }
-    })
+
+    });
 };
 
-
-$(document).ready(function() {
-    listUsers();
-});
 
 
