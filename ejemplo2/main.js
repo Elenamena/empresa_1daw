559b21c679ed889781658c3427bb8976ece6209a
Elena <menaromero.elena@gmail.com>
2025-06-04 11:07:38 +0200

    index ej 2

--- ejemplo2/main.js
+++ ejemplo2/main.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
     $("#cargar").click(function() {
         $.ajax ( {
-            url: "datos.php",
+            url: "https://jsonplaceholder.typicode.com/todos",
             type: "GET",
             success: function(respuesta) {
                 $("#resultado").html(respuesta);
