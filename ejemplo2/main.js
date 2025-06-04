1c87e4d58caabbe2800549e536a918b744ed3a81
Elena <menaromero.elena@gmail.com>
2025-06-04 11:20:44 +0200

    success ej 2

--- ejemplo2/main.js
+++ ejemplo2/main.js
@@ -4,8 +4,17 @@ $(document).ready(function() {
         $.ajax ( {
             url: "https://jsonplaceholder.typicode.com/todos",
             type: "GET",
-            success: function(respuesta) {
-                $("#resultado").html(respuesta);
+            success: function(data) {
+                $("#lista").empty(); 
+
+                for(let i = 0; i < 10; i++) {
+                    const tarea = data[i];
+                    const estado = tarea.completed ? "Completada" : "Pendiente";
+
+                  $("#lista").append(
+                    `<li class="${estado}"><strong>ID:</strong> ${tarea.id} - <strong>Título:</strong> ${tarea.title}(${estado})</li>`
+                  );
+                }
             },
             error: function(error) {
                 $("#resultado").html("Error: " + error);
