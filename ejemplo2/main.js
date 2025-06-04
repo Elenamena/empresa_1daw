f3fff2fbff1f318335c39a48400faa51c50256dd
Elena <menaromero.elena@gmail.com>
2025-06-04 11:42:42 +0200

    ej2

--- ejemplo2/main.js
+++ ejemplo2/main.js
@@ -5,19 +5,19 @@ $(document).ready(function() {
             url: "https://jsonplaceholder.typicode.com/todos",
             type: "GET",
             success: function(data) {
-                $("#lista").empty(); 
+                $("#listaTareas").empty(); 
 
                 for(let i = 0; i < 10; i++) {
                     const tarea = data[i];
                     const estado = tarea.completed ? "Completada" : "Pendiente";
 
-                  $("#lista").append(
+                  $("#listaTareas").append(
                     `<li class="${estado}"><strong>ID:</strong> ${tarea.id} - <strong>Título:</strong> ${tarea.title}(${estado})</li>`
                   );
                 }
             },
             error: function(error) {
-                $("#lista").html("Error: " + error);
+                $("#listaTareas").html("Error: " + error);
             }
         });
     });
