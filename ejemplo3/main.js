2f2c1186240267dd3c2ff4c35930fe0f980cd803
Elena <menaromero.elena@gmail.com>
2025-06-05 10:41:26 +0200

    js ej3

---
+++ ejemplo3/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('#cargar').click(function() {
+        $.ajax( {
+        type: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/users',
+        contentType: 'application/json',
+        async: true,
+        success: function(data) {
+            $('#userList').empty();
+
+            data.forEach(usuario => {
+                $('#userList').append(
+                    `<li><strong>${usuario.name}</strong> (${usuario.email})</li>`
+                );
+            });
+
+            $('#ocultar').show();
+        },
+        error: function(error) {
+            alert('Error al obtener los usuarios');
+        }
+
+    });
+
+    });
+    
+});
+
+
