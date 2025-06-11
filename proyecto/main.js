384dc9e9c888a416b8dc86e3486232ce8a2dfd42
Elena <menaromero.elena@gmail.com>
2025-06-11 11:23:24 +0200

    js

--- proyecto/main.js
+++ proyecto/main.js
@@ -1,5 +1,15 @@
 
+let productosBuscados = [];
+
 $(document).ready(function() {
+    
+    $("#busqueda").on('click', function() { 
+        $("#form").removeClass("d-none");
+        $("#camara").addClass("d-none");
+        $("#resultado").addClass("d-none");
+        $("#error").addClass("d-none").text("");    
+    });
+
 });
 
 
