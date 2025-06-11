6014818a642255f0d157c4c01efed83028ea4991
Elena <menaromero.elena@gmail.com>
2025-06-11 11:38:04 +0200

    js

--- proyecto/main.js
+++ proyecto/main.js
@@ -10,6 +10,29 @@ $(document).ready(function() {
         $("#error").addClass("d-none").text("");    
     });
 
+    $("#escanear").on('click', function() { 
+        $("#form").removeClass("d-none");
+        $("#camara").addClass("d-none");
+        $("#resultado").addClass("d-none");
+        $("#error").addClass("d-none").text("");    
+    });
+
+    $("#form").submit(function(event) {
+        event.preventDefault();
+        let codigo = $("#codigo").val().trim();
+        buscarProducto(codigo);
+    });
+
+    $("#simularCodigo").click(function() {
+        const codigo = "1234";
+        buscarProducto(codigo);
+    });
+
+    $("#reiniciar").click(function() {
+        productosBuscados = [];
+        $("#lista").empty();
+    });
+
 });
 
 
