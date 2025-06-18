4cafa68166a588b8efef8bb5dc7ab1b9ace90d0c
Elena <menaromero.elena@gmail.com>
2025-06-18 12:46:08 +0200

    proy

--- proyecto/main.js
+++ proyecto/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
     let productos = [];
 
-    $("#info").hide("d-none");
+    $("#info").hide();
 
 
     $.getJSON("json.json", function(data) {
@@ -12,6 +12,23 @@ $(document).ready(function() {
 
 
     $("#busqueda").click(function() {
+        buscarProducto();
+    });
+
+
+    $("#codigo").keypress(function(event) {
+        if (event.which === 13) { 
+            buscarProducto();
+        }
+    });
+
+
+    $("#codigo").on("input", function() {
+        $("#info").hide("d-none");
+    });
+
+
+    function buscarProducto() {
         const codigoIntr = $("#codigo").val().trim();
 
         if (codigoIntr === "") {
@@ -19,20 +36,21 @@ $(document).ready(function() {
             return;
         }
 
+        const num = parseInt(codigoIntr);
+
+        if (isNaN(num) || num <= 0 || num > 10) {
+            mostrarError("Error");
+            return;
+        } 
+
         const producto = productos.find(p => p.codigo === codigoIntr);
 
-        if (codigo) {
+        if (producto) {
             mostrarProducto(producto);
         } else {
             mostrarError("Producto no encontrado");
         }
-    });
-
-    $("#codigo").keypress(function(event) {
-        if (event.which === 13) { 
-            mostrarProducto();
-        }
-    });
+    }
 
 
     function mostrarProducto(producto) {
